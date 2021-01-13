069c8765d965c62a329eb5b4b80c00bc57cb0f26
beqotemp <beqotemp@gmail.com>
2021-01-13 18:27:37 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+// JS
+// import './scripts/simblebar.js'
+// import './scripts/glide.js'
+// import './scripts/range-slider.js'
+// import './scripts/sticky.js'
+// import './scripts/slick-slider'
+// import './scripts/azazlyst'
+// import "./scripts/menu";
+// import "./scripts/scrollbar";
+// import "./scripts/slider";
+// import "./scripts/tabs";
+import "./scripts/menu-scroll"
+
+
+// // Vue
+// import Vue from 'vue/dist/vue.common.dev';
+// import VCalendar from 'v-calendar/lib/components/date-picker.umd';
+// new Vue({
+//     el:'#app',
+//     data:{
+//     	date: '',
+//     },
+//     components: {
+//         VCalendar
+//     }
+// })
