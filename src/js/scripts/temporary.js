311f8dd4aa848b249cc1e65ad65d29e33cfbcf3c
beqotemp <beqotemp@gmail.com>
2021-04-04 21:19:37 +0300

    finish?orwhat

--- src/js/scripts/temporary.js
+++ src/js/scripts/temporary.js
@@ -17,21 +17,12 @@ document.getElementById('menu-caller').addEventListener("click",()=>{
 
 
 // modals
-document.getElementById("modal-caller").addEventListener('click', ()=>{
-    document.getElementById("site").classList.add("overflow")
-    document.getElementById("modal").classList.add("active")
-})
 document.getElementById("modal-close").addEventListener('click', ()=>{
     document.getElementById("site").classList.remove("overflow")
     document.getElementById("modal").className = '';
 })
-// document.getElementById("modal-bg").addEventListener('click', ()=>{
-//     document.getElementById("site").classList.remove("overflow")
-//     document.getElementById("modal").classList.remove("active")
-//     enable()
-// })
-
 // Portfolios 
+let fullProjectName = ['Технокультура', 'Eventoutlet', 'Rocket Shina','ПСК Гарант','Balkan LPG Market','Hamcore']
 let PortfolioClasses = ['techno','event','rocket','psk','balcan','hamcore']
 let modal = document.getElementById("modal")
 document.querySelectorAll('.case').forEach((item,event)=>{
@@ -41,7 +32,7 @@ document.querySelectorAll('.case').forEach((item,event)=>{
         modal.classList.add('active')
         modal.classList.add( PortfolioClasses[item.dataset.number])
         document.getElementById("site").classList.add("overflow")
-        
+        document.getElementById("project-name-modal").innerHTML = fullProjectName[item.dataset.number]
     })
 })
 
